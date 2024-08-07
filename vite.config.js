6c7f0c051c9f94b28da586b16007d0f40f551273
amirnsamimi <amirnsamimi@gmail.com>
2024-08-07 19:44:01 +0330

    edit on vite config

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+    outDir: 'dist'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
